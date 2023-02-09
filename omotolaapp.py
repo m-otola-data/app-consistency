821eb3a85af7521d67843fb800a05bf622668305
Omotola Anibaba <46730889+m-otola-data@users.noreply.github.com>
2023-02-08 21:37:42 -0600

    Update omotolaapp.py

--- omotolaapp.py
+++ omotolaapp.py
@@ -9,11 +9,11 @@
 # The purpose of this project is to help developers understand what type of apps are likely to attract more users on Google Play and the App Store. 
 # In doing so, this can provide insight for developers wanting to create an app and generate revenue. 
 
-# A developer could follow this strategy to help ensure App success:
-# 1. Create a minimal Android version of application and add it to Google Play.
-# 2. App will be developed further IF it gets a good response from users
-# 3. If app continues to be profitable after 6 months, an iOS version will be built and added to the App store.
-# This will help further ensure that the app is received well in both markets and generate more revenue.
+ A developer could follow this strategy to help ensure App success:
+ 1. Create a minimal Android version of application and add it to Google Play.
+ 2. App will be developed further IF it gets a good response from users
+ 3. If app continues to be profitable after 6 months, an iOS version will be built and added to the App store.
+ This will help further ensure that the app is received well in both markets and generate more revenue.
 
 
 # In[1]:
